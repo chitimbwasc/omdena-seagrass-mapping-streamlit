5fb95b27b3037dddd7eedfa5c80a2596f682f1fc
Christopher Chitimbwa <chitimbwasc@yahoo.com>
2024-05-28 03:43:56 +0200

    Update pages.py

--- src/pages.py
+++ src/pages.py
@@ -108,6 +108,7 @@ def guide():
     display_image('https://cdn-images-1.medium.com/max/800/0*vBDO0wwrvAIS5e1D.png')
     
     ################### INFORMATION SECTION #######################
+    main_predict()
 
 # Placeholder function
 def datasets():
