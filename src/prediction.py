5c5d177975a296949bc52de4d6bc595ad28ded34
Christopher Chitimbwa <chitimbwasc@yahoo.com>
2024-05-28 03:48:27 +0200

    Update prediction.py

--- src/prediction.py
+++ src/prediction.py
@@ -65,7 +65,7 @@ def main_predict():
         # model = model_load('./saved_models/unet_cleaned_summer_V1.h5')
         model = utils_v2.retrieve_model()
     elif chosen_region == "Croatia":
-        model = model_load('./saved_models/unet_wcc_summer_croatia.h5')
+        # model = model_load('./saved_models/unet_wcc_summer_croatia.h5')
         # src/models/unet_summer_images_augmented_wcc_final.h5
         model = model_load('./models/unet_summer_images_augmented_wcc_final.h5')
 
